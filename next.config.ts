8d012b2ffeae2a4d4c3139c41144c7fa97ff3e85
Teshan Jayakody <teshanjayakody217@gmail.com>
2026-04-05 00:33:39 +0530

    Issue Fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: "export",
+  trailingSlash: true,
+  basePath: "/portfolio-teshan",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
